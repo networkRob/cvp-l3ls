d3ba0664f75ded38f95514b7916709651941c656
Rob Martin <robmartin@arista.com>
2018-06-14 14:57:57 -0500

    revised

--- cvp-L3LS.py
+++ cvp-L3LS.py
@@ -1,6 +1,8 @@
-import sys, jsonrpclib, yaml
+import sys, jsonrpclib
+sys.path.append('/usr/lib64/python2.7/site-packages/')
+import yaml
 from cvplibrary import CVPGlobalVariables as cvpGV
-from cvplibrary import GlobalVriableNames as GVN
+from cvplibrary import GlobalVariableNames as GVN
 
 
 def sendCmd(commands):
@@ -10,7 +12,7 @@ def sendCmd(commands):
     else:
         user = cvpGV.getValue(GVN.CVP_USERNAME)
         passwd = cvpGV.getValue(GVN.CVP_PASSWORD)
-    url = "https://{0}:{1}@{2}/command-api".format(user,passwd,hostip)
+    url = "https://%s:%s@%s/command-api"%(user,passwd,hostip)
     switch = jsonrpclib.Server(url)
     response = switch.runCmds(1,commands)[0]
     return(response)
@@ -22,25 +24,28 @@ yFile = 'hostvars/l3ls.yml'
 smac = cvpGV.getValue(GVN.CVP_MAC)
 ztp = cvpGV.getValue(GVN.ZTP_STATE)
 hostip = cvpGV.getValue(GVN.CVP_IP)
-infoY = yaml.load(yFile)
+infoY = yaml.load(open(yFile))
 shost = sendCmd(['show hostname'])['hostname']
 
 #L3 interfaces
 for r1 in infoY['L3'][shost]:
-    print('interface {0}'.format(r1['interface']))
+    print('interface %s' %(r1['interface']))
+#add check for 0.0.0 and status disabled
     print(' no switchport')
-    print(' ip address {0}/{1}'.format(r1['ip'],r1['cidr']))
-    print(' description {0}'.format(r1['description']))
+    print(' ip address %s/%s' %(r1['ip'],r1['cidr']))
+    print(' description %s' %(r1['description']))
     print('!')
-
-print('router bgp {0}'.format(infoY['BGP'][shost]['as']))
-for r1 in i['BGP'][shost]['bgp_neighbors']:
-    for r2 in i['topology'][shost]['connections']:
+print('ip routing')
+print('!')
+print('router bgp %s' %(infoY['BGP'][shost]['as']))
+print(' maximum-paths 4 ecmp 4')
+for r1 in infoY['BGP'][shost]['bgp_neighbors']:
+    for r2 in infoY['topology'][shost]['connections']:
         if r1['device'] == r2['device']:
             reINT = r2['interface']
-            for r3 in i['L3'][r1['device']]:
+            for r3 in infoY['L3'][r1['device']]:
                 if reINT == r3['interface']:
                     reIP = r3['ip']
-    print(' neighbor {0} remote-as {1}'.format(r1['device'],r1['remote-as']))
+    print(' neighbor %s remote-as %s' %(r1['device'],r1['remote-as']))
 print(' redistribute connected')
 print('!')
