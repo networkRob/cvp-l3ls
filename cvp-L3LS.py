1e062203bae657d05dee7245858faf688fcd3757
Rob Martin <robmartin@arista.com>
2018-06-15 11:44:19 -0500

    Streamlined code and added in for loopback interfaces

--- cvp-L3LS.py
+++ cvp-L3LS.py
@@ -5,53 +5,59 @@ from cvplibrary import CVPGlobalVariables as cvpGV
 from cvplibrary import GlobalVariableNames as GVN
 
 
-def sendCmd(commands):
-    if ztp == True:
-        user = cvpGV.getValue(GVN.ZTP_USERNAME)
-        passwd = cvpGV.getValue(GVN.ZTP_PASSWORD)
-    else:
-        user = cvpGV.getValue(GVN.CVP_USERNAME)
-        passwd = cvpGV.getValue(GVN.CVP_PASSWORD)
-    url = "https://%s:%s@%s/command-api"%(user,passwd,hostip)
-    switch = jsonrpclib.Server(url)
-    response = switch.runCmds(1,commands)[0]
-    return(response)
-
-
 #Variable declarations
 yFile = 'hostvars/l3ls.yml'
 
 smac = cvpGV.getValue(GVN.CVP_MAC)
-ztp = cvpGV.getValue(GVN.ZTP_STATE)
-hostip = cvpGV.getValue(GVN.CVP_IP)
 infoY = yaml.load(open(yFile))
-shost = sendCmd(['show hostname'])['hostname']
+shost = infoY['device'][smac]
+
+#Grabbing sections of the YAML file
+curINT = infoY['topology'][shost]['interfaces']
+curBGP = infoY['topology'][shost]['BGP']
+lcurINT = []
+lloopINT = {}
+
 
 #L3 interfaces
-for r1 in infoY['L3'][shost]:
-    lip = r1['ip']
-    lstatus = r1['status']
-    print('interface %s' %(r1['interface']))
-    if lip == '0.0.0.0':
-        print(' switchport mode access')
-    else:
-        print(' no switchport')
-        print(' ip address %s/%s' %(lip,r1['cidr']))
-    if lstatus == 'disable':
+for r1 in curINT['ethernet']:
+    cid = curINT['ethernet'][r1]
+    lipn = '%s/%s' %(cid['ip'],cid['cidr'])
+    lstatus = cid['status']
+    lcurINT.append((r1,{'ipn':lipn,'status':lstatus,'mode':cid['mode'],'desc':cid['description']}))
+
+lcurINT.sort()
+
+for r1 in lcurINT:
+    print('interface %s' %(r1[0]))
+    if r1[1]['status'] == 'disable':
         print(' shutdown')
-    print(' description %s' %(r1['description']))
+    print(' description %s'%r1[1]['desc'])
+    if r1[1]['mode'] == 'L3':
+        print(' no switchport')
+        print(' ip address %s'%r1[1]['ipn'])
+    else:
+        print(' switchport mode %s'%(r1[1]['mode']))
     print('!')
-print('ip routing')
-print('!')
-print('router bgp %s' %(infoY['BGP'][shost]['as']))
+for r1 in curINT['loopback']:
+    cidl = curINT['loopback']
+    print('interface %s'%r1)
+    llIP = '%s/%s'%(cidl[r1]['ip'],cidl[r1]['cidr'])
+    print(' ip address %s'%llIP)
+    lloopINT[r1] = llIP
+    print('!')
+
+#Start BGP Section
+print('router bgp %s' %(curBGP['as']))
+print(' router-id %s'%(curINT['loopback']['Loopback0']['ip']))
 print(' maximum-paths 4 ecmp 4')
-for r1 in infoY['BGP'][shost]['bgp_neighbors']:
-    for r2 in infoY['topology'][shost]['connections']:
-        if r1['device'] == r2['device']:
-            reINT = r2['interface']
-            for r3 in infoY['L3'][r1['device']]:
-                if reINT == r3['interface']:
-                    reIP = r3['ip']
-    print(' neighbor %s remote-as %s' %(r1['device'],r1['remote-as']))
-print(' redistribute connected')
+for r1 in curBGP['bgp_neighbors']:
+    rede = r1[0]
+    reint = r1[1]
+    reas = infoY['topology'][rede]['BGP']['as']
+    reip = infoY['topology'][rede]['interfaces']['ethernet'][reint]['ip']
+    print(' neighbor %s remote-as %s' %(reip,reas))
+    print(' neighbor %s maximum-routes 12000'%(reip))
+for r1 in lloopINT:
+    print(' network %s'%lloopINT[r1])
 print('!')
